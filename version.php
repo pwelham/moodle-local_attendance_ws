92c36b55375edf04cf794f12632640426cbb1d4f
Joe Souch <p0091841@brookes.ac.uk>
2024-03-20 13:27:30 +0000

    Version bump

--- version.php
+++ version.php
@@ -27,10 +27,10 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_attendance_ws';
-$plugin->version = 2024011600;
+$plugin->version = 2024032000;
 $plugin->requires = 2012120301;
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = 'v1.1.0'; //Optional - Human-readable version name
+$plugin->release = 'v1.2.0'; //Optional - Human-readable version name
 $plugin->dependencies = array(
     'local_obu_timetable_usergroups' => 2024010800,
     'local_obu_metalinking' => 2024012300
