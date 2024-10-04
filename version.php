dbca69581b37e8bb7850e0ea413c2d7d05d1d88f
Joe Souch <p0091841@brookes.ac.uk>
2024-10-04 09:48:52 +0100

    include room label prefix

--- version.php
+++ version.php
@@ -27,7 +27,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_attendance_ws';
-$plugin->version = 2024100102;
+$plugin->version = 2024100103;
 $plugin->requires = 2012120301;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->release = 'v1.4.1';
